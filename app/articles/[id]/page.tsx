a9ddb5c4c3b865a7e71f6e2d6c482bb981b65e4c
Shunta-Iwasaki <niwaka0219@gmail.com>
2025-02-07 21:27:44 +0900

    修正_頑張って。。

--- app/articles/[id]/page.tsx
+++ app/articles/[id]/page.tsx
@@ -1,28 +1,31 @@
 import articles from '@/data/articles.json';
-import{ Article } from '@/types/article';
+import { Article } from '@/types/article';
 import LikeButton from '@/components/LikeButton';
 
-export async function generateStaticParams(): Promise<{ id: string}[]> {
+// 動的ルートのパスを生成
+export async function generateStaticParams(): Promise<{ id: string }[]> {
     return articles.map((article: Article) => ({
         id: article.id.toString(),
     }));
 }
 
-interface PageProps {
-    params: {id: string };
-}
+// Next.js の型定義
+type ArticleDetailProps = {
+    params: { id: string };
+};
 
-export default function ArticleDetail({ params }: PageProps) {
+export default function ArticleDetail({ params }: ArticleDetailProps) {
     const article = articles.find((a) => a.id.toString() === params.id);
 
     if (!article) {
-        return <div>記事が見つかりませんでした。</div>
+        return <div>記事が見つかりませんでした。</div>;
     }
+
     return (
         <div>
             <h1>{article.title}</h1>
             <div>{article.content}</div>
             <LikeButton />
         </div>
-    )
+    );
 }
